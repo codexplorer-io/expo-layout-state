a738815284ee41aec2ecb6d14b413e29e53d3045
Marko Matic <mail.marko.matic@gmail.com>
2023-05-19 11:21:29 +1000

    - Initial impl.

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import isEqual from 'lodash/isEqual';
+
+const areLayoutsEquals = (
+    {
+        x,
+        y,
+        width,
+        height
+    },
+    {
+        x: currentX,
+        y: currentY,
+        width: currentWidth,
+        height: currentHeight
+    }
+) => isEqual(
+    {
+        x,
+        y,
+        width,
+        height
+    },
+    {
+        x: currentX,
+        y: currentY,
+        width: currentWidth,
+        height: currentHeight
+    }
+);
+
+export const useLayout = ({ width } = { width: 0 }) => {
+    const [currentLayout, setCurrentLayout] = useState({
+        x: 0,
+        y: 0,
+        width,
+        height: 0
+    });
+
+    return {
+        currentLayout,
+        setCurrentLayout: layout => {
+            !areLayoutsEquals(layout, currentLayout) && setCurrentLayout(layout);
+        }
+    };
+};
+
+export const OnLayout = ({
+    children
+}) => {
+    const [currentLayout, setCurrentLayout] = useState({
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0
+    });
+    return children({
+        setLayout: layout => {
+            !areLayoutsEquals(layout, currentLayout) && setCurrentLayout(layout);
+        },
+        layout: currentLayout
+    });
+};
